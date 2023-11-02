e7e68dc137855eabaaeb56ee24f63466365041d0
LuanCKRG <crackersage@gmail.com>
2023-11-02 01:46:44 -0400

    style(eslint): fix lint issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Lumyns Games',
-  description: 'Working to think something',
+  title: "Lumyns Games",
+  description: "Working to think something",
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
